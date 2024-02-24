e9c36d9ac860311359685a734c260ab7179cbf26
Shariful <sharifaiub15@gmail.com>
2024-02-24 02:46:39 +0100

    dynamic data load/modal added

--- scripts/phone.js
+++ scripts/phone.js
@@ -1,25 +1,27 @@
-const loadPhone = async(searchText) => {
+const loadPhone = async(searchText, isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
-    displayPhones(phones);
+    displayPhones(phones, isShowAll);
 }
 
-const displayPhones = phones => {
+const displayPhones = (phones, isShowAll) => {
     // 1. get the container
     const phoneContainer = document.getElementById('phone-container');
     // clear the container
     phoneContainer.textContent = '';
     // display show all button
     const showAllContainer = document.getElementById('show-all-container');
-    if(phones.length > 12) {
+    if(phones.length > 12  && !isShowAll) {
         showAllContainer.classList.remove('hidden');
     }
     else{
         showAllContainer.classList.add('hidden');
     }
     // reduce phones array
-    phones = phones.slice(0, 12);
+    if(!isShowAll){
+        phones = phones.slice(0, 12);
+    }
     phones.forEach((phone => {
         // 2. create a div
         const phoneCard = document.createElement('div');
@@ -27,13 +29,12 @@ const displayPhones = phones => {
         // 3. set innerHTML
         phoneCard.innerHTML =   `
         <figure class="px-10 pt-10">
-            <img src="${phone.image}" alt="Shoes" class="rounded-xl" />
+            <img src="${phone.image}" alt="" class="rounded-xl" />
         </figure>
         <div class="card-body items-center text-center">
             <h2 class="card-title">${phone.phone_name}</h2>
-            <p>${phone.slug}</p>
             <div class="card-actions">
-                <button class="btn btn-primary">Buy Now</button>
+                <button onclick="handleShowDetails('${phone.slug}')" class="btn btn-primary">Show details</button>
             </div>
         </div>
         `;
@@ -45,11 +46,19 @@ const displayPhones = phones => {
 }
 
 
-const handleSearch = () => {
+// show details
+const handleShowDetails = async(id) => {
+    // load individual data
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+}
+
+
+const handleSearch = (isShowAll) => {
     toggleLoadingSpinner(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    loadPhone(searchText);
+    loadPhone(searchText, isShowAll);
 };
 
 // handle search spinner
@@ -62,3 +71,8 @@ const toggleLoadingSpinner = (isLoading) => {
         loadingSpinner.classList.add('hidden');
     }
 };
+
+
+const handleShowAll = () =>{
+    handleSearch(true);
+}
